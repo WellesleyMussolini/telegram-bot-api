0173eeb0ee64622af2a7d92d3b861750bbc059de
Wellesley Mussolini <iei.muss@gmail.com>
2024-11-20 00:12:44 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import bodyParser from "body-parser";
+const telegramBot = require("node-telegram-bot-api");
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+const bot = new telegramBot(process.env.TOKEN, { polling: true });
+
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// Route to send a message
+app.post("/send-message", async (req: any, res: any) => {
+  const { chatId, message } = req.body;
+
+  // Validate input
+  if (!chatId || !message) {
+    console.error("Missing chatId or message in the request body.");
+    return res.status(400).json({ error: "chatId and message are required!" });
+  }
+
+  try {
+    console.log(`Sending message: "${message}" to chatId: ${chatId}`);
+    const response = await bot.sendMessage(chatId, message);
+    console.log("Telegram API Response:", response);
+
+    res
+      .status(200)
+      .json({ success: true, message: "Message sent successfully!" });
+  } catch (error: any) {
+    console.error("Error sending message to Telegram:", error.message);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("Hello, TypeScript Node Express!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log("Server is running");
+
+  // bot.on("message", (msg: any) => {
+  //   const chatId = msg.chat.id;
+  //   console.log("Received message:", msg.text, "Chat ID:", chatId);
+  // });
+});
